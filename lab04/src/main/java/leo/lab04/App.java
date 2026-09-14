395df3d8814dfdb52bf206252f28b1cfd357b683
Leo <>
2026-09-14 09:48:13 -0400

    Add Git Repository link

--- lab04/src/main/java/leo/lab04/App.java
+++ lab04/src/main/java/leo/lab04/App.java
@@ -1,18 +1,19 @@
+// https://github.com/Lewl07/Lab4.git
+
 package leo.lab04;
 
-import java.util.Scanner;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
 /**
  * JavaFX App
+ * https://github.com/Lewl07/Lab4.git
  */
 public class App extends Application {
 
