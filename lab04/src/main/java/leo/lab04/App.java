bdb60f8acea2c22e12a6b24014cb806507d19643
Leo <>
2026-09-14 09:32:14 -0400

    Set up the labels and text fields in the application for user input

--- lab04/src/main/java/leo/lab04/App.java
+++ lab04/src/main/java/leo/lab04/App.java
@@ -20,15 +20,45 @@ public class App extends Application {
         GridPane gridPane = new GridPane();
         
         Label daysOnTrip = new Label("Number of days on the trip: ");
-        Label amountOfAirfare = new Label("Amount of airfare, if any: ");
-        Label amountCarRentalFees = new Label("Amount of car rental fees, if any: ");
-        Label numOfMiles = new Label("Number of miles driven, if a private vehicle was used: ");
-        Label parkingFees = new Label("Amount of parking fees, if any: ");
-        Label taxiCharges = new Label("Amount of taxi charges, if any: ");
-        Label conferenceFees = new Label("Conference or seminar registration fees, if any: ");
-        Label lodgingCharges = new Label("Lodging charges, per night: ");
+        TextField daysOnTripTxt = new TextField();
+        gridPane.add(daysOnTrip, 0, 0);
+        gridPane.add(daysOnTripTxt, 1, 0);
         
- 
+        Label amountOfAirfare = new Label("Amount of airfare, if any: ");
+        TextField amountOfAirfareTxt = new TextField();
+        gridPane.add(amountOfAirfare, 0, 1);
+        gridPane.add(amountOfAirfareTxt, 1, 1);
+        
+        Label amountCarRentalFees = new Label("Amount of car rental fees, if any: ");
+        TextField amountCarRentalFeesTxt = new TextField();
+        gridPane.add(amountCarRentalFees, 0, 2);
+        gridPane.add(amountCarRentalFeesTxt, 1, 2);
+        
+        Label numOfMiles = new Label("Number of miles driven, if a private vehicle was used: ");
+        TextField numOfMilesTxt = new TextField();
+        gridPane.add(numOfMiles, 0, 3);
+        gridPane.add(numOfMilesTxt, 1, 3);
+        
+        Label parkingFees = new Label("Amount of parking fees, if any: ");
+        TextField parkingFeesTxt = new TextField();
+        gridPane.add(parkingFees, 0, 4);
+        gridPane.add(parkingFeesTxt, 1, 4);
+        
+        Label taxiCharges = new Label("Amount of taxi charges, if any: ");
+        TextField taxiChargesTxt = new TextField();
+        gridPane.add(taxiCharges, 0, 5);
+        gridPane.add(taxiChargesTxt, 1, 5);
+        
+        Label conferenceFees = new Label("Conference or seminar registration fees, if any: ");
+        TextField conferenceFeesTxt = new TextField();
+        gridPane.add(conferenceFees, 0, 6);
+        gridPane.add(conferenceFeesTxt, 1, 6);
+        
+        Label lodgingCharges = new Label("Lodging charges, per night: ");
+        TextField lodgingChargesTxt = new TextField();
+        gridPane.add(lodgingCharges, 0, 7);
+        gridPane.add(lodgingChargesTxt, 1, 7); 
+        
         Scene scene = new Scene(gridPane);
         stage.setScene(scene);
         stage.show();
